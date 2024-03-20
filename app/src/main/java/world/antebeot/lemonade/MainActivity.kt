bdcc3dd827cd59796476a0a1fa6a57fc33e4a43e
WipedLifePotato <startyourday60@gmail.com>
2024-03-20 12:01:04 +0700

    to center

--- app/src/main/java/world/antebeot/lemonade/MainActivity.kt
+++ app/src/main/java/world/antebeot/lemonade/MainActivity.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -22,8 +24,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import world.antebeot.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -91,7 +95,7 @@ enum class lemonState(val value: Int)
             3 -> "Нажми на лимонад выпить"
             else -> "Стакан пуст"
         }
-        Text(text, modifier=modifier)
+        Text(text, modifier=modifier, fontSize = 18.sp, textAlign = TextAlign.Center)
     }
     fun isAllowStateOfLemon(state: Int) = state in min_value..max_value
 }
@@ -99,18 +103,22 @@ enum class lemonState(val value: Int)
 fun LemonadeImageAntText(modifier: Modifier = Modifier) {
     var lemonadeState by remember { mutableStateOf (lemonState.LEMON_TREE) }
 
-    Column {
-        lemonadeState.CurrentLemonImage()
-        lemonadeState.CurrentLemonText()
-        Button(modifier = Modifier,
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
             onClick = {
                 val lemonStateNext = lemonadeState.getNextState()
                 lemonadeState = lemonStateNext
-            }) {
-            Text("Нажать на лимон", modifier = Modifier)
+            }
+        ) {
+            lemonadeState.CurrentLemonImage()
         }
+        Spacer(modifier = Modifier.height(32.dp))
+        lemonadeState.CurrentLemonText()
     }
-
 }
 
 @Preview(showBackground = true)
